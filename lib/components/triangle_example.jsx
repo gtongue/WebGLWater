f60f5aed8bea525ba81e6d428e70bc4421de87df
Garrett Tongue <garrettjtongue@gmail.com>
2017-12-06 00:16:19 -0800

    Get plane rendering with moving verts over time and start refactor

--- lib/components/triangle_example.jsx
+++ lib/components/triangle_example.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Water from '../water';
 import vertexShader from '../shaders/simple_vs';
 import fragmentShader from '../shaders/simple_fs';
+import othervertex from '../shaders/timed_vs';
 
 export default class TriangleExample extends React.Component {
   constructor(props){
@@ -12,19 +13,19 @@ export default class TriangleExample extends React.Component {
   componentDidMount(){
     let canvas = this.refs.canvas;
     let gl = canvas.getContext("webgl");
-    canvas.width = 1920;
-    canvas.height = 1080;
+    canvas.width = 1280;
+    canvas.height = 720;
 
     gl = canvas.getContext('webgl');
     gl.viewport(0,0,gl.canvas.width, gl.canvas.height);
 
-    this.Water = new Water(vertexShader, fragmentShader, gl);
+    this.Water = new Water(vertexShader, fragmentShader, othervertex, gl);
   }
 
   render(){
     return (
       <div>
-        <canvas ref = "canvas" style = {{width: "1000px", height: "1000px"}}>
+        <canvas ref = "canvas" style = {{width: "1280px", height: "720px"}}>
 
         </canvas>
       </div>
